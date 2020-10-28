87ac5f9ef61e5a294f3a7cfb104070e5f6d4d80d
Dmitry Afonchenko <undermove@yandex.ru>
2020-10-28 19:15:17 +0300

    екн ещ сщттусе зкщвгсук фтв сщтыгьук

--- KafkaConsumer/Program.cs
+++ KafkaConsumer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using Confluent.Kafka;
 
 namespace KafkaConsumer
 {
@@ -6,7 +8,34 @@ namespace KafkaConsumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "127.0.0.1:9093",
+                GroupId = "foo",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Thread consumerThread = new Thread(o =>
+            {
+                using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+            
+                consumer.Subscribe("my-topic");
+
+                while (!cts.IsCancellationRequested)
+                {
+                    var consumeResult = consumer.Consume(cts.Token);
+                
+                    Console.WriteLine($"Consumed message: {consumeResult.Message}");
+                }
+            
+                consumer.Close();
+            });
+
+            consumerThread.Start();
+            Console.ReadLine();
+            cts.Cancel();
+            consumerThread.Abort();
         }
     }
 }
--- KafkaProducer/Program.cs
+++ KafkaProducer/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Confluent.Kafka;
 
 namespace KafkaProducer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9093",
+                ClientId = Dns.GetHostName()
+            };
+            
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+            producer.Produce(
+                "my-topic", new Message<Null, string> { Value = "hello world" }, report =>
+                {
+                    if (report.Error != null)
+                    {
+                        Console.WriteLine($"Error: {report.Error}");
+                        return;
+                    }
+                    
+                    Console.WriteLine($"Offset: {report.Offset}");
+                });
+
+            Console.ReadLine();
         }
     }
 }
