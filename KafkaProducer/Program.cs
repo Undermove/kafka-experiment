439eb74d063046e9a91944770076f9fd4ac04d82
Dmitry Afonchenko <undermove@yandex.ru>
2020-11-07 01:47:20 +0300

    Add rabbit consumers

--- KafkaProducer/Program.cs
+++ KafkaProducer/Program.cs
@@ -1,37 +1,60 @@
 ﻿using System;
 using System.Net;
 using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 using Confluent.Kafka;
 
 namespace KafkaProducer
 {
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            var config = new ProducerConfig
-            {
-                BootstrapServers = "localhost:9092",
-                SecurityProtocol = SecurityProtocol.Plaintext,
-                ClientId = Dns.GetHostName()
-            };
-            // TODO: https://hub.docker.com/r/bitnami/kafka/
-            
-            using var producer = new ProducerBuilder<Null, string>(config).Build();
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var summary = BenchmarkRunner.Run<KafkaProducerClassBenchmark>();
+		}
+	}
+	
+	public class KafkaProducerClass
+	{
+		public void Produce()
+		{
+			var config = new ProducerConfig
+			{
+				BootstrapServers = "localhost:9092",
+				SecurityProtocol = SecurityProtocol.Plaintext,
+				ClientId = Dns.GetHostName()
+			};
 
-            producer.Produce(
-                "my-topic", new Message<Null, string> { Value = "hello world" }, report =>
-                {
-                    if (report.Error != null)
-                    {
-                        Console.WriteLine($"Error: {report.Error}");
-                        return;
-                    }
-                    
-                    Console.WriteLine($"Offset: {report.Offset}");
-                });
+			using var producer = new ProducerBuilder<Null, string>(config).Build();
 
-            Console.ReadLine();
-        }
-    }
+
+			producer.Produce(
+				"my-topic", new Message<Null, string> {Value = "hello world"}, report =>
+				{
+					if (report.Error.Code != ErrorCode.NoError)
+					{
+						Console.WriteLine($"Error: {report.Error}");
+						return;
+					}
+
+					Console.WriteLine($"Offset: {report.Offset}");
+				});
+
+			producer.Flush();
+		}
+	}
+		
+	public class KafkaProducerClassBenchmark
+	{
+		private readonly KafkaProducerClass _kafkaProducer;
+			
+		public KafkaProducerClassBenchmark()
+		{
+			_kafkaProducer = new KafkaProducerClass();
+		}
+
+		[Benchmark]
+		public void ProducingBenchmark() => _kafkaProducer.Produce();
+	}
 }
